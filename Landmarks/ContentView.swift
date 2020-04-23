127c3b1e13776f988507a52b01c4540eaee37dd2
daniel <dmaixner93@gmail.com>
2020-04-23 10:01:42 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Daniel Maixner on 4/23/20.
+//  Copyright © 2020 Daniel Maixner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
